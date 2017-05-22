c4a02735388403c3c0e2c793b95667fc8a5dee16
mikejianzhang <mikejianzhang@163.com>
2017-05-22 23:14:21 +0800

    Add initial version of packit.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='packit',
+    version='1.0',
+    description='Download artifacts from artifactory and generate product package.',
+    author='Mike Zhang',
+    packages=find_packages(),
+)
