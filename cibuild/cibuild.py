952d6a5caa3a83540a6ca4115d16f411ec318e19
mikejianzhang <mikejianzhang@163.com>
2017-05-30 23:11:22 +0800

    Add more build script.

--- cibuild/cibuild.py
+++ cibuild/cibuild.py
@@ -8,10 +8,24 @@ class CIBuild:
     def __init__(self, workDir, prodVersion):
         self._workDir = wordDir
         self._prodVersion = prodVersion
+        self._buildInfo = {}
         self._pathStack = PathStackMgr()
+        self.generateBuildInfo()
 
     def prebuild(self):
-        return
+        self.createLabel()
+        return (self._buildInfo['build.number'],
+                self._buildInfo['build.version'],
+                self._buildInfo['build.label'],
+                self._buildInfo['build.commit'])
+
+    def generateBuildInfo(self):
+        sNextNumber = str(self.getNextBuildNumber())
+        sBuildVersion = self._prodVersion + "_b" + sNextNumber
+        self._buildInfo['build.number'] = sNextNumber;
+        self._buildInfo['build.version'] = sBuildVersion
+        self._buildInfo['build.label'] = sBuildVersion
+        self._buildInfo['build.commit'] = self.getCurrentCommit()
 
     def createLabel(self):
         return
@@ -19,4 +33,7 @@ class CIBuild:
     def getNextBuildNumber(self):
         iNextBN = 1
         output1 = subprocess.check_output(['git','tag','-l', tagPrefix_s1 + '_rc_' + '*','--sort=-version:refname'], shell=True)
+        return iNextBN
+
+    def getCurrentCommit(self):
         return
